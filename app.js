547e1849f48a13abb7751d5c2e5ed1437a67df70
dkk934 <dkking934@gmail.com>
2026-01-02 14:10:42 +0530

    new chatbot template- openAI

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import chatRoute from "./chat.js";
+
+// Load environment variables from .env
+dotenv.config();
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+// Parse URL-encoded bodies (for form submissions)
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/chat", chatRoute);
+
+app.listen(port, () => console.log(`http://localhost:${port}`));
